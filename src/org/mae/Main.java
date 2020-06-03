bd15a1b6961aa8c9a796b5931958f0bf696d8894
magpiech <drmordillo@gmail.com>
2020-06-03 18:11:03 +0200

    additional test checkout added

--- src/org/mae/Main.java
+++ src/org/mae/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	//https://github.com/MrEcrm/DemoIntellijHeute write your code here
-        System.out.println("Hello World!!* ha");
+        System.out.println("Hello World!!* ha test checkout");
     }
 }
