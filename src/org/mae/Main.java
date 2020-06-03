1271e68f7d3f07804f2b95e3b9c7ccea8c8879a8
magpiech <drmordillo@gmail.com>
2020-06-03 18:00:02 +0200

    additional ! added

--- src/org/mae/Main.java
+++ src/org/mae/Main.java
@@ -3,7 +3,7 @@ package org.mae;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!");
+	//https://github.com/MrEcrm/DemoIntellijHeute write your code here
+        System.out.println("Hello World!!");
     }
 }
